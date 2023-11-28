2ebff2b8ce571e47ff25c42c3d3038be699e3e60
Anastasia7Si <120373288+Anastasia7Si@users.noreply.github.com>
2023-11-28 20:32:57 +0300

    docker

--- db_sql/database.py
+++ db_sql/database.py
@@ -1,9 +1,9 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base, sessionmaker
-from config import DB_HOST, DB_NAME, DB_PASS, DB_USER
+from config import DB_HOST, DB_NAME, DB_PASS, DB_USER, DB_PORT
 
 
-SQLALCHEMY_DATABASE_URL = f'postgresql://{DB_USER}:{DB_PASS}@{DB_HOST}/{DB_NAME}'
+SQLALCHEMY_DATABASE_URL = f'postgresql://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}'
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL,
