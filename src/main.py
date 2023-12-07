c3c127cef4553b655965f8af820efd2fb93d4a07
Anastasia7Si <120373288+Anastasia7Si@users.noreply.github.com>
2023-12-07 11:45:38 +0300

    deploy with cors ready

--- src/main.py
+++ src/main.py
@@ -5,18 +5,11 @@ from .company.router import router as company_router
 from .dealers.router import router as dealers_router
 
 origins = [
-    "http://localhost",
-    "http://localhost:8080",
-    "http://localhost:8001",
-    "http://localhost:5173",
-    "http://127.0.0.1:8080",
-    "http://127.0.0.1:8001",
-    "http://127.0.0.1:5173",
-    "http://127.0.0.1:1",
-    "http://localhost:80",
-    "http://127.0.0.1:80",
-    "http://localhost:8000",
-    "http://127.0.0.1:8000"
+    "http://proseptmatching.zapto.org:3000",
+    "http://proseptmatching.zapto.org:8080",
+    "http://proseptmatching.zapto.org:8001",
+    "http://proseptmatching.zapto.org:5173",
+    "http://proseptmatching.zapto.org"
 ]
 
 
@@ -27,8 +20,11 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,
     allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
+    allow_methods=["GET", "POST", "OPTIONS", "DELETE", "PATCH", "PUT"],
+    allow_headers=["Content-Type", "Set-Cookie",
+                   "Access-Control-Allow-Headers",
+                   "Access-Control-Allow-Origin",
+                   "Authorization"],
 )
 
 
